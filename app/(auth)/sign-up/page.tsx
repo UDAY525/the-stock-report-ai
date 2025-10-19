ac926f53591514d7e64aa6028b4b1d581640d1e5
uday <udaybikkinauday54359@gmail.com>
2025-10-19 16:38:17 +0530

    authentication mails synchronization implemented

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -7,6 +7,7 @@ import SelectField from "@/components/forms/SelectField";
 import {INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS} from "@/lib/constants";
 import {CountrySelectField} from "@/components/forms/CountrySelectField";
 import FooterLink from "@/components/forms/FooterLink";
+import {signUpWithEmail} from "@/lib/actions/auth.actions";
 import {useRouter} from "next/navigation";
 import {toast} from "sonner";
 
@@ -32,7 +33,8 @@ const SignUp = () => {
 
     const onSubmit = async (data: SignUpFormData) => {
         try {
-            console.log('Submitting...');
+            const result = await signUpWithEmail(data);
+            if(result.success) router.push('/');
         } catch (e) {
             console.error(e);
             toast.error('Sign up failed', {
